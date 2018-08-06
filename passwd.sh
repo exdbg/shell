38f8a8e4acf07be260bed4bca1856fc716523a18
abc0012383 <282404250@qq.com>
2018-08-06 15:01:26 +0800

    Update passwd.sh

--- passwd.sh
+++ passwd.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
-#Author:Wesker Lu
-#Version 1.0
 NUM1=10000000
 NUM2=3600
 NUM3=24
@@ -10,7 +8,7 @@ if [ -s yad ];then
         > yad
 fi
 
-/usr/bin/ldapsearch -H ldap://xxx.com -Y GSSAPI -N -b ou=Users,ou=China,ou=Corporate,dc=xxx,dc=com '(&(objectClass=user)(userAccountControl=512))'|grep pwdLastSet|awk -F ":" '{print $2}'> day
+/usr/bin/ldapsearch -H ldap://xxx.com -Y GSSAPI -N -b ou=xxx,dc=xxx,dc=com '(&(objectClass=user)(userAccountControl=512))'|grep pwdLastSet|awk -F ":" '{print $2}'> day
 
 while read line
         do
@@ -23,22 +21,17 @@ while read line
                 echo $LAST >> yad
 done < day
 
-/usr/bin/ldapsearch -H ldap://xxx.com -Y GSSAPI -N -b ou=Users,ou=China,ou=Corporate,dc=xxx,dc=com '(&(objectClass=user)(userAccountControl=512))'|grep userPrincipalName|awk -F ":" '{print $2}' > email
+/usr/bin/ldapsearch -H ldap://xxx.com -Y GSSAPI -N -b ou=xxx,dc=xxx,dc=com '(&(objectClass=user)(userAccountControl=512))'|grep userPrincipalName|awk -F ":" '{print $2}' > email
 
-paste -d : email yad > join
+paste -d " " email yad > join
 
 while read i k
         do
-                if [[ "$k" -lt "5" && "$k" -gt "0" ]];then
+        if [[ "$k" -lt 5 && "$k" -gt 0 ]];then
+         echo -e "Hi,\nYour xxx domain passord will exprie within $k days, please consider change your password ASAP.\n" | mail -s 'Password Expiration Reminder' $i
 
-                        /usr/sbin/sendmail -t -F Password Check << EOF
-                        SUBJECT:Your Linux & Domain password will expried soon
-                        TO:$i
-                        Your Linux & Domain passord will expried in $k days, please change your password ASAP.
-
-                        IT
-
-EOF
                 fi
 done < join
+
+exit 0
 
