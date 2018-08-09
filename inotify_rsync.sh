fccbb79b272ef3a245b370493361e4395fb23e10
root <abc0012383>
2018-08-09 11:55:18 +0800

    add new

---
+++ inotify_rsync.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+host=172.16.43.47
+user=root
+src='/tmp/src1/'
+dest='/tmp/dest1'
+inotifywait -mrq -e modify,attrib,moved_to,moved_from,move,move_self,create,delete,delete_self --timefmt='%d/%m/%y %H:%M' --format='%T %w%f %e' $src | while read chgeFile
+do
+rsync -avPz --delete $src $user@$host:$dest &>>./rsync.log
+done
+
